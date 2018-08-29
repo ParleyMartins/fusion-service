8cde46a2111248e77d4f22da53f144d61d9069da
Parley Martins <parleypachecomartins@gmail.com>
2018-08-29 19:43:07 -0300

    Add main configurations

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+    faUser,
+    faPhone,
+    faAt,
+    faTimes,
+    faTimesCircle,
+    faPlusCircle,
+    faArrowLeft,
+    faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import router from './router';
+import store from './store';
+
+library.add(faUser, faPhone, faAt, faTimes, faTimesCircle, faPlusCircle, faArrowLeft, faTrash);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    render: h => h(App),
+}).$mount('#app');
